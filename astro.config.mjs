db0c6685613f449c3d90d0be2108407aff3d8d54
Diego Garro <diegogarro1994@gmail.com>
2026-06-03 12:53:35 -0600

    Update astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  output: "static"
+  output: "static",
+  vite: {
+    preview: {
+      allowedHosts: [
+        "smartgymcr.onrender.com",
+        "smartgymcr.com",
+        "www.smartgymcr.com"
+      ]
+    }
+  }
 });
